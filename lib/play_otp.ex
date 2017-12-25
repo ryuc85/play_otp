6aa0ea5d61cdca90ed4f90b30e35fb8844453b0b
ryuc85 <kinr.chou@nsij.org>
2017-12-26 08:42:27 +0900

    Initial commit

---
+++ lib/play_otp.ex
@@ -0,0 +1,18 @@
+defmodule PlayOTP do
+  @moduledoc """
+  Documentation for PlayOTP.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PlayOTP.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/play_otp_test.exs
@@ -0,0 +1,8 @@
+defmodule PlayOTPTest do
+  use ExUnit.Case
+  doctest PlayOTP
+
+  test "greets the world" do
+    assert PlayOTP.hello() == :world
+  end
+end
